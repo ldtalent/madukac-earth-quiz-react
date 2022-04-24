45fcf7e940d29eedfcfcf325a9ba6db3f241b031
Prisca Chidimma Maduka <88077456+Priceless-P@users.noreply.github.com>
2022-04-24 23:24:46 +0100

    Update Learn.js

--- src/component/Learn.js
+++ src/component/Learn.js
@@ -22,7 +22,8 @@ const Learn = () => {
       {Facts &&
         Facts.map((fact) => {
           return (
-            <Box sx={{ flexGrow: 1 }} key={fact.id}>
+            <Box sx={{ flexGrow: 1 }} key={fact.id} style={{
+              marginTop:50}}>
               <Grid
                 container
                 spacing={2}
@@ -30,7 +31,7 @@ const Learn = () => {
                 justifyContent="center"
                 style={{ margin: "auto" }}
               >
-                <Grid item xs={8}>
+                <Grid item xs={8} >
                   <Item>
                     <Typography
                       variant="h5"
